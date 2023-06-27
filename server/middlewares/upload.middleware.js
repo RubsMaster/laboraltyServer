70d7531bd8aac895d3ab17ce4965628191a6f4a5
Hugo <huedlup@gmail.com>
2023-06-27 15:09:03 -0700

    cambios minimos

--- server/middlewares/upload.middleware.js
+++ server/middlewares/upload.middleware.js
@@ -1,6 +1,6 @@
 import { connectDB } from "../db.js";
 import util from "util";
-import multer from "multer";
+import multer  from "multer";
 import { GridFsStorage } from "multer-gridfs-storage";
 import path from "path";
 import fs from 'fs';
